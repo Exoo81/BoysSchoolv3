7de4a6e4bc68bc0ed9704d38d25daeaef768c881
Exoo <marcinpiskoroff@gmail.com>
2018-10-29 13:16:24 +0000

    40.0 	- main loader - implementation (db)

---
+++ public/js/loader/main-loader.js
@@ -0,0 +1,17 @@
+/* 
+ * Main loader controller
+ */
+
+$(window).ready(function() {
+               
+    function show_popup(){
+                   
+        $(".loader-wrapper").fadeOut();
+            
+    };
+    window.setTimeout( show_popup, 1000 ); // 2 seconds
+
+            
+});
+
+
